a3e1aab8b99274986bbbbaf1610562641665d30c
Cornelius A. Ludmann <dev@cornelius-ludmann.de>
2021-04-21 21:33:42 +0000

    Wed 21 Apr 2021 09:33:42 PM UTC

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # containerd-bermuda-gap
+
+## Push images to the registry
+
+```
+docker-compose up -d registry
+docker pull alpine
+docker tag alpine localhost:5000/alpine:latest
+docker push localhost:5000/alpine:latest
+docker-compose down
+```
--- client/main.go
+++ client/main.go
@@ -3,31 +3,55 @@ package main
 import (
 	"context"
 	"log"
+	"os"
+	"strings"
+
 	"time"
 
 	"github.com/containerd/containerd"
 	"github.com/containerd/containerd/namespaces"
+	"github.com/containerd/containerd/remotes/docker"
 )
 
+var DEFAULT_REF = "docker.io/library/alpine:latest"
+var PLAIN_HTTP_RESOLVER = docker.NewResolver(docker.ResolverOptions{PlainHTTP: true})
+
 func main() {
+	log.Println("Sleeping ...")
 	time.Sleep(10 * time.Second)
-	if err := pullImage("docker.io/library/alpine:latest"); err != nil {
-		log.Fatal(err)
+	ref := DEFAULT_REF
+	if len(os.Args) > 1 {
+		ref = os.Args[1]
+	}
+	log.Printf("Starting using image '%s' ...\n", ref)
+
+	for {
+		if err := pullImage(context.Background(), ref); err != nil {
+			log.Fatal(err)
+		}
+		time.Sleep(1 * time.Second)
 	}
 }
 
-func pullImage(ref string) error {
+func pullImage(ctx context.Context, ref string) error {
 	client, err := containerd.New("/run/containerd/containerd.sock")
 	if err != nil {
 		return err
 	}
 	defer client.Close()
-	ctx := namespaces.WithNamespace(context.Background(), "client")
-	image, err := client.Pull(ctx, ref, containerd.WithPullUnpack)
+
+	ctx2 := namespaces.WithNamespace(ctx, "client")
+
+	var image containerd.Image
+	if strings.HasPrefix(ref, "registry:5000") {
+		image, err = client.Pull(ctx2, ref, containerd.WithPullUnpack, containerd.WithResolver(PLAIN_HTTP_RESOLVER))
+	} else {
+		image, err = client.Pull(ctx2, ref, containerd.WithPullUnpack)
+	}
 	if err != nil {
 		return err
 	}
-	log.Printf("Successfully pulled %s image\n", image.Name())
+	log.Printf("Successfully pulled '%s' image.\n", image.Name())
 
 	return nil
 }
