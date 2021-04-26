10509f5f86ba7bd3751e3920e8574eb252682aa6
Cornelius A. Ludmann <dev@cornelius-ludmann.de>
2021-04-26 08:29:22 +0000

    add sleep to manifest.go

--- facade/blob.go
+++ facade/blob.go
@@ -11,7 +11,7 @@ import (
 func (reg *Registry) handleBlob(ctx context.Context, r *http.Request) http.Handler {
 	spname, name := getSpecProviderName(ctx)
 	reference := getReference(ctx)
-	log.Printf("handleBlob – unknown spec provider – spname: %s – name: %s – reference: %s", spname, name, reference)
+	log.Printf("handleBlob – spname: %s – name: %s – reference: %s", spname, name, reference)
 	target, _ := url.Parse("http://localhost:5000/")
 	proxy := httputil.NewSingleHostReverseProxy(target)
 	return proxy
--- facade/manifest.go
+++ facade/manifest.go
@@ -6,12 +6,15 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"time"
 )
 
 func (reg *Registry) handleManifest(ctx context.Context, r *http.Request) http.Handler {
 	spname, name := getSpecProviderName(ctx)
 	reference := getReference(ctx)
-	log.Printf("handleManifest – unknown spec provider – spname: %s – name: %s – reference: %s", spname, name, reference)
+	log.Printf("handleManifest – spname: %s – name: %s – reference: %s", spname, name, reference)
+	time.Sleep(10 * time.Second)
+	log.Printf("handleManifest – end of sleep – spname: %s – name: %s – reference: %s", spname, name, reference)
 	target, _ := url.Parse("http://localhost:5000/")
 	proxy := httputil.NewSingleHostReverseProxy(target)
 	return proxy
